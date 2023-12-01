39fd169e13d8b7e84bbe31ff77e234a2cd552299
Paolo-Federle <federlepaolo95@gmail.com>
2023-12-01 18:24:37 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -35,70 +35,72 @@ import MummyMerits from './pages/Mummy/MummyMerits';
 import Mortal from './pages/MortalsAndOthers/Mortal';
 import MortalMerits from './pages/MortalsAndOthers/MortalMerits';
 import Books from './pages/Generale/Books';
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import './css/App.css';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/books" element={<Books />} />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
 
-        {/* Mortals */}
-        <Route path="/mortal" element={<Mortal />} />
-        <Route path="/mortal/merits" element={<MortalMerits />} />
+          {/* Mortals */}
+          <Route path="/mortal" element={<Mortal />} />
+          <Route path="/mortal/merits" element={<MortalMerits />} />
 
-        {/* VAMPIRI */}
-        <Route path="/vampire" element={<Vampire />} />
-        <Route path="/vampire/disciplines" element={<Disciplines />} />
-        <Route path="/vampire/merits" element={<VampireMerits />} />
+          {/* VAMPIRI */}
+          <Route path="/vampire" element={<Vampire />} />
+          <Route path="/vampire/disciplines" element={<Disciplines />} />
+          <Route path="/vampire/merits" element={<VampireMerits />} />
 
-        {/* WEREWOLF */}
-        <Route path="/werewolf" element={<Werewolf />} />
-        <Route path="/werewolf/gifts " element={<Gifts />} />
-        <Route path="/werewolf/rites" element={<Rites />} />
-        <Route path="/werewolf/merits" element={<WerewolfMerits />} />
+          {/* WEREWOLF */}
+          <Route path="/werewolf" element={<Werewolf />} />
+          <Route path="/werewolf/gifts" element={<Gifts />} />
+          <Route path="/werewolf/rites" element={<Rites />} />
+          <Route path="/werewolf/merits" element={<WerewolfMerits />} />
 
-        {/* MAGE */}
-        <Route path="/mage" element={<Mage />} />
-        <Route path="/mage/spells" element={<Spells />} />
-        <Route path="/mage/merits" element={<MageMerits />} />
-        <Route path="/mage/path" element={<Path />} />
-        <Route path="/mage/order" element={<Order />} />
-        <Route path="/mage/legacy" element={<Legacy />} />
+          {/* MAGE */}
+          <Route path="/mage" element={<Mage />} />
+          <Route path="/mage/spells" element={<Spells />} />
+          <Route path="/mage/merits" element={<MageMerits />} />
+          <Route path="/mage/path" element={<Path />} />
+          <Route path="/mage/order" element={<Order />} />
+          <Route path="/mage/legacy" element={<Legacy />} />
 
-        {/* PROMETEANI */}
-        <Route path="/promethean" element={<Promethean />} />
-        <Route path="/promethean/transmutations" element={<Transmutations />} />
-        <Route path="/promethean/merits" element={<PrometheanMerits />} />
+          {/* PROMETEANI */}
+          <Route path="/promethean" element={<Promethean />} />
+          <Route path="/promethean/transmutations" element={<Transmutations />} />
+          <Route path="/promethean/merits" element={<PrometheanMerits />} />
 
-        {/* CHANGELING */}
-        <Route path="/changeling" element={<Changeling />} />
-        <Route path="/changeling/contracts" element={<Contracts />} />
-        <Route path="/changeling/oaths" element={<Oaths />} />
-        <Route path="/changeling/merits" element={<ChangelingMerits />} />
+          {/* CHANGELING */}
+          <Route path="/changeling" element={<Changeling />} />
+          <Route path="/changeling/contracts" element={<Contracts />} />
+          <Route path="/changeling/oaths" element={<Oaths />} />
+          <Route path="/changeling/merits" element={<ChangelingMerits />} />
 
-        {/* HUNTER */}
-        <Route path="/hunter" element={<Hunter />} />
-        <Route path="/hunter/endowments " element={<Endowments />} />
-        <Route path="/hunter/tactics" element={<Tactics />} />
-        <Route path="/hunter/merits" element={<HunterMerits />} />
+          {/* HUNTER */}
+          <Route path="/hunter" element={<Hunter />} />
+          <Route path="/hunter/endowments " element={<Endowments />} />
+          <Route path="/hunter/tactics" element={<Tactics />} />
+          <Route path="/hunter/merits" element={<HunterMerits />} />
 
-        {/* GEIST */}
-        <Route path="/geist" element={<Geist />} />
-        <Route path="/geist/keys_and_haunts" element={<KeysAndHaunts />} />
-        <Route path="/geist/ceremonies" element={<Ceremonies />} />
-        <Route path="/geist/merits" element={<GeistMerits />} />
+          {/* GEIST */}
+          <Route path="/geist" element={<Geist />} />
+          <Route path="/geist/keys_and_haunts" element={<KeysAndHaunts />} />
+          <Route path="/geist/ceremonies" element={<Ceremonies />} />
+          <Route path="/geist/merits" element={<GeistMerits />} />
 
-        {/* MUMMY */}
-        <Route path="/mummy" element={<Mummy />} />
-        <Route path="/mummy/affinities" element={<Affinities />} />
-        <Route path="/mummy/utterances" element={<Utterances />} />
-        <Route path="/mummy/merits" element={<MummyMerits />} />
-      </Routes>
+          {/* MUMMY */}
+          <Route path="/mummy" element={<Mummy />} />
+          <Route path="/mummy/affinities" element={<Affinities />} />
+          <Route path="/mummy/utterances" element={<Utterances />} />
+          <Route path="/mummy/merits" element={<MummyMerits />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
