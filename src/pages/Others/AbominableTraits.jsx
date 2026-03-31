264211b08dac58defab72cf447b2a1561e5cc920
Paolo-Federle <108668774+Paolo-Federle@users.noreply.github.com>
2026-03-31 21:27:46 +0200

    adominable traits

--- src/pages/Others/AbominableTraits.jsx
+++ src/pages/Others/AbominableTraits.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import SimpleTable from '../../components/SimpleTable';
-import { AbominableMeritsData } from '../../Data/Others/AbominableTraitsData';
+import { AbominableMeritsData, AbominableRitesData, AbominablePowersData } from '../../Data/Others/AbominableTraitsData';
 
 
 export default function AbominableTraits() {
     return (
         <div className='grid-container'>
             <h1 className="cursor-pointer text-2xl flex items-center gap-1 ">Abominable Merits</h1>
-            <p className='max-w-screen-lg'>The following two Merits grant cultists and supplicants of otherworldly entities the capacity to wield inhuman, inexplicable powers.
-                These Traits should be available primarily to Storytellercontrolled characters, being available to players’ characters only with Storyteller approval.</p>
+            <p className='max-w-screen-lg'>
+                The following two Merits grant cultists and supplicants of otherworldly entities the capacity to wield inhuman, inexplicable powers.
+                These Traits should be available primarily to Storytellercontrolled characters, being available to players’ characters only with Storyteller approval.
+            </p>
             <SimpleTable table={AbominableMeritsData} />
+            <SimpleTable table={AbominableRitesData} title={'Abominable rites'}/>
+            <SimpleTable table={AbominablePowersData} title={'Abominable powers'}/>
         </div>
     );
 }
