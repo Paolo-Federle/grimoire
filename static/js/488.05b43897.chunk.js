2dab36976f6953686aa4812a2ddd62692e02e1d1
Paolo-Federle <108668774+Paolo-Federle@users.noreply.github.com>
2026-04-17 00:29:17 +0200

    Updates

--- static/js/488.05b43897.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgrimoire=self.webpackChunkgrimoire||[]).push([[488],{488(e,i,k){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.05b43897.chunk.js.map
---
+++ static/js/488.e9e7a574.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgrimoire=self.webpackChunkgrimoire||[]).push([[488],{107(e,i,k){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.e9e7a574.chunk.js.map
